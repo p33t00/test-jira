fab60ee5a49da6a38831bebbd11fd1c243a68a37
p33t <p33t00@gmail.com>
2022-03-29 22:27:56 +0200

    testing unit tests init commit

---
+++ src/main/java/Pete.java
@@ -0,0 +1,5 @@
+public class Pete {
+    int incBy2(int i) {
+        return i * 2;
+    }
+}
---
+++ src/test/java/TestPete.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestPete {
+    @Test
+    public void testIncBy2() {
+        Pete p = new Pete();
+        int res = p.incBy2(2);
+        assertEquals(1, res);
+    }
+}
